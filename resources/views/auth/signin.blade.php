91e7c6f8d50950ed9302a85c34c7bbe20c388fc6
oscartoomey <oscartoomey10@gmail.com>
2022-09-26 14:33:37 +0100

    Update signin.blade.php

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.layout')
 
 @php
-        header('Location: ' . 'https://accounts.spotify.com/authorize?client_id=5ff3da8d355e43ffb5e1a8a521932728&redirect_uri=https%3A%2F%2Ffinalyearprojectoscar.herokuapp.com%2Fspotify&response_type=code&scope=playlist-read-private+user-read-private+user-read-recently-played&state=12d7af5432436d28');
+        header('Location: ' . 'https://accounts.spotify.com/authorize?client_id=5ff3da8d355e43ffb5e1a8a521932728&redirect_uri=https%3A%2F%2Flocalhost%3A80%2Fproject%2Fpublic%2Fspotify&response_type=code&scope=playlist-read-private+user-read-private+user-read-recently-played&state=12d7af5432436d28');
         die();
 @endphp
