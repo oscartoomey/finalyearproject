0475e970093e17760a8b1fc246d7577c0af4a94a
oscartoomey <oscartoomey10@gmail.com>
2022-09-26 14:45:14 +0100

    Update stravaSignIn.blade.php

--- resources/views/auth/stravaSignIn.blade.php
+++ resources/views/auth/stravaSignIn.blade.php
@@ -2,6 +2,6 @@
 
 @php
 
-header("Location: https://www.strava.com/oauth/authorize?client_id=80033&response_type=code&redirect_uri=http://localhost:80/project/public/stravaAuth&approval_prompt=force&scope=activity:read_all");
+header("Location: https://www.strava.com/oauth/authorize?client_id=80033&response_type=code&redirect_uri=http://localhost:80/stravaAuth&approval_prompt=force&scope=activity:read_all");
 die();
 @endphp
